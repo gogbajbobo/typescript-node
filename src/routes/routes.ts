f9082b38e39d2f824d37b58bac58f91f04de67fd
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-24 13:07:54 +0300

    route('/test')

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,19 @@ import { Router } from 'express';
 const router = Router();
 
 router.route('/')
-    .get((req, res) => res.status(200).json({ error: false, message: 'OK!', uri: process.env.SCRIPT_URI || 'localhost' }));
+    .get((req, res) => {
+
+        const jsonData: Object = {
+            error: false,
+            message: 'OK!',
+            uri: process.env.SCRIPT_URI || 'localhost'
+        };
+        res.status(200).json(jsonData);
+
+    });
+
+router.route('/test')
+    .get((req, res) => res.render('index'));
+
 
 export { router };
