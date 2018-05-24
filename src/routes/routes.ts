db1613bcc61ecd45ab9f02eb9d49895295cdd6ed
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-24 07:33:08 +0300

    add process.env.SCRIPT_URI to response

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,6 @@ import { Router } from 'express';
 const router = Router();
 
 router.route('/')
-    .get((req, res) => res.status(200).json({ error: false, message: 'OK!' }));
+    .get((req, res) => res.status(200).json({ error: false, message: 'OK!', uri: process.env.SCRIPT_URI || 'localhost' }));
 
 export { router };
